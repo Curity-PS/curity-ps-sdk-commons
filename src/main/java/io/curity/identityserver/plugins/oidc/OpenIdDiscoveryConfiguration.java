8dd35bf602982a9c935637e49389b713e0598f75
Daniel Lindau <d.lindau@gmail.com>
2025-05-14 12:21:39 +0200

    feat: Managed object for using OpenID metadata

---
+++ src/main/java/io/curity/identityserver/plugins/oidc/OpenIdDiscoveryConfiguration.java
@@ -0,0 +1,19 @@
+package io.curity.identityserver.plugins.oidc;
+
+import se.curity.identityserver.sdk.config.Configuration;
+import se.curity.identityserver.sdk.config.annotation.Description;
+import se.curity.identityserver.sdk.service.HttpClient;
+import se.curity.identityserver.sdk.service.Json;
+
+import java.net.URI;
+
+public interface OpenIdDiscoveryConfiguration extends Configuration
+{
+    @Description("The issuer URI of the OpenID Connect provider. This will be used to discover the OpenID Connect configuration, as well as for validation.")
+    URI getIssuer();
+
+    @Description("The HTTP client to use for making requests to the OpenID Connect provider to collect the metadata.")
+    HttpClient getHttpClient();
+
+    Json getJson();
+}
---
+++ src/main/java/io/curity/identityserver/plugins/oidc/OpenIdDiscoveryManagedObject.java
@@ -0,0 +1,61 @@
+package io.curity.identityserver.plugins.oidc;
+
+import io.curity.identityserver.plugins.jwt.JwksUriJwtValidator;
+import io.curity.identityserver.plugins.jwt.JwtValidator;
+import io.curity.identityserver.plugins.utils.NullUtils;
+import se.curity.identityserver.sdk.config.Configuration;
+import se.curity.identityserver.sdk.http.HttpResponse;
+import se.curity.identityserver.sdk.plugin.ManagedObject;
+import se.curity.identityserver.sdk.service.HttpClient;
+
+import java.net.URI;
+import java.util.Map;
+
+/**
+ * A managed object for OpenID Discovery. Requires a {@link OpenIdDiscoveryConfiguration} with configuration of the metadata.
+ * Also fetches the JWKS found in the metadata. Server configuration is config scoped
+ */
+public final class OpenIdDiscoveryManagedObject<T extends Configuration> extends ManagedObject<T>
+{
+    private final Map<String, Object> _discoveredConfiguration;
+    private final OpenIdDiscoveryConfiguration _openIdDiscoveryConfiguration;
+    private final JwksUriJwtValidator _jwtValidator;
+
+    public OpenIdDiscoveryManagedObject(T configuration, OpenIdDiscoveryConfiguration openIdDiscoveryConfiguration)
+    {
+        super(configuration);
+        _openIdDiscoveryConfiguration = openIdDiscoveryConfiguration;
+        HttpClient httpClient = openIdDiscoveryConfiguration.getHttpClient();
+        _discoveredConfiguration = fetchProviderConfiguration(openIdDiscoveryConfiguration.getIssuer(), httpClient);
+        _jwtValidator = new JwksUriJwtValidator(URI.create(getConfiguredString("jwks_uri")), httpClient);
+    }
+
+    private Map<String, Object> fetchProviderConfiguration(URI issuer, HttpClient httpClient)
+    {
+        return httpClient.request(issuer.resolve(".well-known/openid-configuration"))
+                .get()
+                .response()
+                .body(HttpResponse.asJsonObject(_openIdDiscoveryConfiguration.getJson()));
+    }
+
+    private String getConfiguredString(String key)
+    {
+        return NullUtils.valueOfTypeOrError(String.class, _discoveredConfiguration.get(key),
+                "Did not find " + key + " in provider metadata");
+    }
+
+    public URI getTokenEndpoint()
+    {
+        return URI.create(getConfiguredString("token_endpoint"));
+    }
+
+    public URI getAuthorizeEndpoint()
+    {
+        return URI.create(getConfiguredString("authorization_endpoint"));
+    }
+
+    public JwtValidator getJwtValidator()
+    {
+        return _jwtValidator;
+    }
+}
